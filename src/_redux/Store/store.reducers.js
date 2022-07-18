d723ad29bc68673eb745f66a955e75c33d46146e
rove24 <enriquezrovelin@gmail.com>
2022-07-18 21:16:41 +0800

    added initial sample product

--- src/_redux/Store/store.reducers.js
+++ src/_redux/Store/store.reducers.js
@@ -7,7 +7,26 @@ const initialState = {
   //   isReading: false,
   //   isUpdating: false
   // },
-  products: [],
+
+  products: [
+    //sample products
+    {
+      name: 'ADILETTE',
+      price: 2600,
+      category: ['slides', 'new'],
+      description: 'Women Originals',
+      long_description:
+        'The moment you step into these adidas Adilette Ayoon Slides, you are at ease. That is the whole point of them. They let you bring the comfort of home with you anywhere, thanks to the super soft one-piece EVA foam build. That minimalist approach extends beyond the construction — the colours also keep things toned down, which happens to mean that they also style well with everything.'
+    },
+    {
+      name: 'AQUA',
+      price: 1200,
+      category: ['slides', 'new'],
+      description: 'Core Black',
+      long_description:
+        'An iconic style for slick surfaces. These quick-drying slides flash 3-Stripes across the bandage-style upper. Plush cushioning in the footbed provides the ultimate in comfort in and out of the shower.'
+    }
+  ],
   active_product: 0
 };
 
